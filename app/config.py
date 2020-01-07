68f9d24f3d660b16a0cf034d4c834ca256dad1fc
Seongmin <lll2slll@naver.com>
2020-01-07 14:50:50 +0900

    Oracle 연결 추가

--- app/config.py
+++ app/config.py
@@ -3,20 +3,43 @@ import os
 DEBUG = os.environ.get("DEBUG", True)
 HOST = os.environ.get("HOST", "127.0.0.1")
 PORT = os.environ.get("PORT", 4000)
+DB_TYPE = os.environ.get("PROTOCOL", "oracle")
 SECRET_KEY = "flasknotewithsqlalchemy"
 
+# MySql
 mysql_config = {
+    "protocol": "mysql",
     "user": os.environ.get("DB_USER", "root"),
     "pass": os.environ.get("DB_PASS", "1234"),
     "host": os.environ.get("DB_HOST", "localhost"),
     "db": os.environ.get("DB_NAME", "mysql"),
 }
 
-SQLALCHEMY_DATABASE_URI = "mysql://%s:%s@%s/%s?charset=utf8" % (
+mysql_uri = "%s://%s:%s@%s/%s?charset=utf8" % (
+    mysql_config["protocol"],
     mysql_config["user"],
     mysql_config["pass"],
     mysql_config["host"],
     mysql_config["db"],
 )
+
+# Oracle
+oracle_config = {
+    "protocol": "oracle",
+    "user": os.environ.get("DB_USER", "root"),
+    "pass": os.environ.get("DB_PASS", "1234"),
+    "host": os.environ.get("DB_HOST", "localhost"),
+    "db": os.environ.get("DB_NAME", "ORCL"),
+}
+
+oracle_uri = "%s://%s:%s@%s/%s" % (
+    oracle_config["protocol"],
+    oracle_config["user"],
+    oracle_config["pass"],
+    oracle_config["host"],
+    oracle_config["db"],
+)
+
+SQLALCHEMY_DATABASE_URI = DB_TYPE == "oracle" and oracle_uri or mysql_uri
 SQLALCHEMY_ECHO = True
 SQLALCHEMY_TRACK_MODIFICATIONS = False
