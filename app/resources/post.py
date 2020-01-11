aa3fb5ca3651e98f2d3e6e1683dcfa83c71f35ad
Seongmin <lll2slll@naver.com>
2020-01-11 21:27:34 +0900

    CRUD 추가, Model에 생성자 추가

--- app/resources/post.py
+++ app/resources/post.py
@@ -1,12 +1,28 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 from models.post import Post
+from models import db
+
+parser = reqparse.RequestParser()
+parser.add_argument('content')
 
 
 class PostListController(Resource):
     def get(self):
-        posts = Post.query.all()
+        # posts = Post.query.all()
+        # posts = Post.query.limit(2).all()
+        # posts = Post.query.filter(Post.content.like('%Post%')).all()
+        # posts = Post.query.filter_by(content="Post").all()
+        posts = Post.query.order_by(Post.id.desc()).all()
 
         return [dict(post.to_dict()) for post in posts]
+
+    def post(self):
+        args = parser.parse_args()
+        post = Post(args['content'])
+        db.session.add(post)
+        db.session.commit()
+
+        return post.to_dict()
 
 
 class PostController(Resource):
@@ -17,3 +33,19 @@ class PostController(Resource):
             return "Not found", 404
 
         return post.to_dict()
+
+    def patch(self, post_id):
+        post = Post.query.get(post_id)
+
+        post.content = 'Edit Text'
+        db.session.commit()
+
+        return post.to_dict()
+
+    def delete(self, post_id):
+        post = Post.query.get(post_id)
+
+        db.session.delete(post_id)
+        db.session.commit()
+
+        return 202
